23180c1bf68d6336c2e1a78808edc2919965c885
VicSergeev <vicsergeevrov@icloud.com>
2025-01-26 17:04:04 +0300

    init

---
+++ MeteorShowers/Models/MeteorShower.swift
@@ -0,0 +1,19 @@
+//
+//  MeteorShower.swift
+//  MeteorShowers
+//
+//  Created by Vic on 26.01.2025.
+//
+
+import Foundation
+
+struct MeteorShower {
+    let name: String
+    let dateBegin: Date
+    let datePeak: Date
+    let dateEnd: Date
+    let description: String
+    let parent: String
+    let radiant: String
+    let zhr: String
+}
---
+++ MeteorShowers/ViewControllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeteorShowers
+//
+//  Created by Vic on 26.01.2025.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
