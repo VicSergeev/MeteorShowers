ba7a16ddcb6153400f6ef6934de4e07c1f57b079
VicSergeev <vicsergeevrov@icloud.com>
2025-01-29 16:50:46 +0300

    Update ShowerDetailViewController.swift
    setup ui elements

--- MeteorShowers/ViewControllers/ShowerDetailViewController.swift
+++ MeteorShowers/ViewControllers/ShowerDetailViewController.swift
@@ -11,7 +11,7 @@ import SnapKit
 final class ShowerDetailViewController: UIViewController {
     
     // MARK: - Main stack
-    lazy private var stackView: UIStackView = {
+    lazy private var mainStackView: UIStackView = {
         var stackView = UIStackView()
         stackView.axis = .vertical
         stackView.spacing = 8
@@ -20,11 +20,67 @@ final class ShowerDetailViewController: UIViewController {
         
         return stackView
     }()
+    
+    // MARK: - Main stack
+    lazy private var contentStackView: UIStackView = {
+        var stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        
+        return stackView
+    }()
+    
+    lazy private var titleLabel: UILabel = {
+        var label = UILabel()
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy private var mainImageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.image = UIImage(systemName: "moon")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    lazy private var notificationIcon: UIImageView = {
+        var imageView = UIImageView()
+        imageView.image = UIImage(systemName: "bell.fill")
+        imageView.tintColor = .systemGreen
+        imageView.contentMode = .scaleAspectFit
+        imageView.snp.makeConstraints { make in
+            make.height.width.equalTo(24)
+        }
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
+}
+
+extension ShowerDetailViewController {
+    func setupUI() {
+        view.addSubview(mainStackView)
+        
+        mainStackView.addArrangedSubview(titleLabel)
+        mainStackView.addArrangedSubview(notificationIcon)
+        mainStackView.addArrangedSubview(contentStackView)
+        
+        contentStackView.addArrangedSubview(mainImageView)
+        
+        
+        mainImageView.snp.makeConstraints { make in
+            make.leading.trailing.top.bottom.equalToSuperview()
+        }
+        
+        contentStackView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().offset(12)
+        }
+    }
 }
